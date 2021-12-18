eff7ea143e37d0f81c88f97fd5c437405a9c6c93
Osnago-L <osna.brera@gmail.com>
2021-12-18 18:30:25 +0100

    end project

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,8 @@
+// PRIMO SNACK 
 
 function functPali(){
     let palindromo = prompt("Inserire una parola per verificare se è palindroma...");
-    palindromo=palindromo.toLowerCase()
+    palindromo = palindromo.toLowerCase()
 
     let arrayPalindromo = palindromo.split("");
     let arrayLastHalf = [];
@@ -26,5 +27,55 @@ function functPali(){
         
     }
     
+
+};
+
+// SECONDO SNACK 
+
+function ParieDisp(){
+
+    // CREAZIONI VARIABILI UTENTE 
+    let pariDispari = prompt("Pari o Dispari?");
+    pariDispari = pariDispari.toLowerCase();
+
+    if((pariDispari != "dispari")&&(pariDispari != "pari")){
+        let randomUserOddEven = Math.floor(Math.random()*2+1);
+        if(randomUserOddEven == 1){
+            pariDispari= "pari";
+        alert(`Vabbe questa volta scelgo io, "hai scelto" ${pariDispari}`);
+        }else{
+            pariDispari= "dispari";
+            alert(`Vabbe questa volta scelgo io, "hai scelto" ${pariDispari}`);
+        }
+    }
+
+    let userNumber = prompt("Inserisci un numero da 1 a 5...");
+    if(userNumber>5){
+        userNumber = Math.floor(Math.random()*5+1);
+        alert(`Vabbe questa volta scelgo io, il numero "da te scelto" è ${userNumber}` );
+    }
+    // CREAZIONE VARIABILE AI 
+    function randomAI(){
+        let randomAINumber = Math.floor(Math.random()*5+1);
+        return randomAINumber;
+    };
+    // VARIABILE SOMMA 
+    function somma(){
+        let somma = randomAI() + userNumber;
+        if (somma%2 == 0){
+            let pari = "pari";
+            return pari ;
+        }else{
+            let dispari = "dispari";
+            return dispari ;
+        }
+    
+    };
+
+    if(somma() == pariDispari){
+        alert("player WINS :)")
+    }else{
+        alert("player LOSES :(")
+    }
 
 };
